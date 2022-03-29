7f3cf722d8ddb0f652cdd45a4b50a8952a60eac6
Daverson Saldanha <dds.saldanha@gmail.com>
2022-03-29 14:37:51 -0300

    Comunicação API

---
+++ src/pages/api/clientes/index.js
@@ -0,0 +1,16 @@
+export default function cliente(req, res) {
+    
+    if (req.method === "GET") {
+        handleGET(req, res);
+    } else {
+        res.status(405).send()
+    }
+}
+
+function handleGET(req, res) {
+    res.status(200).json({
+        id: 3,
+        nome: 'Maria',
+        idade: 33
+    })
+}
